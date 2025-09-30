bb50d13853ad2cc9662767efe142d6150c9c0593
Willian Pramio <willianpramio@hotmail.com>
2025-09-30 17:21:06 +0000

    Cache results of 'get_courses' call

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,15 +3,17 @@ require_relative "../services/teachable_api"
 class CoursesController < ApplicationController
   # GET /courses
   def index
-    @courses = TeachableApi.get_courses.map do |course_data|
-      Course.new(
-        id: course_data["id"].to_i,
-        name: course_data["name"],
-        heading: course_data["heading"],
-        description: course_data["description"],
-        is_published: course_data["is_published"],
-        image_url: course_data["image_url"]
-      )
+    @courses = Rails.cache.fetch("courses_list", expires_in: 5.minutes) do
+      TeachableApi.get_courses.map do |course_data|
+        Course.new(
+          id: course_data["id"].to_i,
+          name: course_data["name"],
+          heading: course_data["heading"],
+          description: course_data["description"],
+          is_published: course_data["is_published"],
+          image_url: course_data["image_url"]
+        )
+      end
     end
   end
 
